99af8051d730db6452228c0b79b5f80b9754ddbb
Xiaokai Xia <dddd1007@gmail.com>
2023-07-19 16:59:09 +0800

    Fix bug

--- xia_eeg_toolkit/__init__.py
+++ xia_eeg_toolkit/__init__.py
@@ -165,8 +165,8 @@ def generate_evokes(data_cache: dict, condition_list: list):
         condition_list: A list containing the conditions to be processed.
 
     Returns:
-        If the condition list has only one element, it returns a list where each element 
-        is the ERP corresponding to the given condition.
+        If the condition list has only one element, it returns a list where each 
+        element is the ERP corresponding to the given condition.
         If the condition list has more than one element, it returns a dictionary where 
         the keys are the conditions and the values are a list where each element is the 
         ERP corresponding to that condition.
@@ -231,11 +231,11 @@ def show_difference_wave(evokes_diff, chan_name, axes=None):
                                                         axes=axes,
                                                         show_sensors='upper right',
                                                         combine='mean',
-                                                        title=chan_name + "Difference Wave")
+                                                        title=chan_name + "Difference Wave")  # noqa: E501
     return difference_wave_plot
 
 
-def calc_erp_ttest(data_cache: dict, condition_list: list, time_window: list, 
+def calc_erp_ttest(data_cache: dict, condition_list: list, time_window: list,
                    direction: str, ch_name='eeg'):
     if ch_name == "eeg":
         ch_nums = 64
@@ -246,7 +246,10 @@ def calc_erp_ttest(data_cache: dict, condition_list: list, time_window: list,
     condition = condition_list[0]
     cond1_array = np.zeros((ch_nums, len(data_cache)))
     for i, processed_data in enumerate(data_cache.values()):
-        evoke_data = processed_data[condition].average().get_data(tmin=time_window[0], tmax=time_window[1])
+        evoke_data = processed_data[condition].average()
+        if ch_name != "eeg":
+            evoke_data = evoke_data.pick_channels([ch_name])
+        evoke_data = evoke_data.get_data(tmin=time_window[0], tmax=time_window[1])
         evoke_mean = evoke_data.mean(axis=1)
         cond1_array[:, i] = evoke_mean
 
@@ -254,7 +257,10 @@ def calc_erp_ttest(data_cache: dict, condition_list: list, time_window: list,
     condition = condition_list[1]
     cond2_array = np.zeros((ch_nums, len(data_cache)))
     for i, processed_data in enumerate(data_cache.values()):
-        evoke_data = processed_data[condition].average().get_data(tmin=time_window[0], tmax=time_window[1])
+        evoke_data = processed_data[condition].average()
+        if ch_name != "eeg":
+            evoke_data = evoke_data.pick_channels([ch_name])
+        evoke_data = evoke_data.get_data(tmin=time_window[0], tmax=time_window[1])
         evoke_mean = evoke_data.mean(axis=1)
         cond2_array[:, i] = evoke_mean
 
@@ -271,7 +277,7 @@ def calc_erp_ttest(data_cache: dict, condition_list: list, time_window: list,
         ci_95 = se_diff * np.array(t.interval(0.95, df))
 
         tt_results.append([tt_result.statistic, tt_result.pvalue,
-                           ci_95, tt_result.pvalue < 0.05])
+                           ci_95[0], ci_95[1], tt_result.pvalue < 0.05])
 
     if ch_name == "eeg":
         # Just take the channel names from the last processed data
@@ -280,25 +286,8 @@ def calc_erp_ttest(data_cache: dict, condition_list: list, time_window: list,
         chan_name = [ch_name]
 
     df = pd.DataFrame(tt_results, columns=['T-Statistic', 'P-Value',
-                                           '95% CI', 'Significant'])
+                                           '95% CI Lower', '95% CI Upper', 
+                                           'Significant'])
     df.index = chan_name
 
     return df
-
-    # # ttest
-    # tt_results = np.zeros(cond1_array.shape[0])
-    # for chan_num in range(cond1_array.shape[0]):
-    #     cond1_data = cond1_array[chan_num - 1, :]
-    #     cond2_data = cond2_array[chan_num - 1, :]
-    #     foo = ttest_rel(cond1_data, cond2_data, alternative=direction)
-    #     tt_results[chan_num - 1] = foo[1]
-
-    # if ch_name == "eeg":
-    #     chan_name = mne.read_epochs(processed_data_list[i])[
-    #         condition].info['ch_names'][0:63]
-    # else:
-    #     chan_name = ch_name
-
-    # tt_results_dict = dict(zip(chan_name, tt_results))
-
-    # return tt_results_dict
