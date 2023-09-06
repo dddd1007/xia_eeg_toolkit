853d91ed1a7e1150846670e0f63551653edf9b35
Xiaokai Xia <dddd1007@gmail.com>
2023-09-06 18:32:57 +0800

    Fix bug

--- xia_eeg_toolkit/__init__.py
+++ xia_eeg_toolkit/__init__.py
@@ -1,7 +1,19 @@
-from .erp import load_data, generate_evokes, generate_diff_evokes, generate_mean_evokes, compare_evoke_wave, \
-    show_difference_wave, calc_erp_ttest
-from .freq import perform_time_frequency_analysis, average_time_frequency_analysis, calculate_time_frequency_for_all, \
-    plot_avg_power, anova_power_over_all
+from .erp import (
+    load_data,
+    generate_evokes,
+    generate_diff_evokes,
+    generate_mean_evokes,
+    compare_evoke_wave,
+    show_difference_wave,
+    calc_erp_ttest,
+)
+from .freq import (
+    perform_time_frequency_analysis,
+    average_time_frequency_analysis,
+    calculate_time_frequency_for_all,
+    plot_avg_power,
+    anova_power_over_all,
+)
 from .lrp import compute_lrp, compute_and_average_lrp, compute_group_average_lrp
 from .preprocess import preprocess_epoch_data
-from .single_trials import single_trial_analysis, load_epochs, load_reject_log, load_sub_beh_data
+from .single_trials import *
